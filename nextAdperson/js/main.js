4883a58505ed2d4faba0a5504ac97d7e346dd6b0
Kim dami <s3327550@gmail.com>
2025-08-21 18:37:00 +0900

    250821 발표자료

--- nextAdperson/js/main.js
+++ nextAdperson/js/main.js
@@ -1,26 +1,40 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage({
-		anchors: ['firstSection', 'secondSection', 'thirdSection','fourthSection'],
-		sectionsColor: ['transparent', 'transparent', '#101010'],
 		sectionSelector: '.vertical-scrolling',
 		navigation: true,
 		controlArrows: false,
 		css3: true,
 		scrollingSpeed: 700,
-		afterLoad: function(origin, destination, direction, trigger){
-			if (destination == 1){
-				$("#Main .areaTitle .firstTitle").addClass("up");
-				$("#Main .areaTitle .secondTitle").addClass("up");
+		afterLoad: function(anchorLink, index){
+			if (index == 1){
+				$("#section1").addClass("step1");
 			} else{
-				$("#Main .areaTitle .firstTitle").removeClass("up");
-				$("#Main .areaTitle .secondTitle").removeClass("up");
+				$("#section1").removeClass("step1");
 			}
 
-			
-			if (destination == 2){ 
-				$("#Opening .areaOpening").addClass("up");
+			if(index == 2){
+                setTimeout(function() {
+					$("#section2").addClass("step1");
+				}, 0);
+                setTimeout(function() {
+					$("#section2").addClass("step2");
+				}, 150);
+                setTimeout(function() {
+					$("#section2").addClass("step3");
+				}, 300);
+                setTimeout(function() {
+					$("#section2").addClass("step4");
+				}, 1200);
+                setTimeout(function() {
+					$("#section2").addClass("step5");
+				}, 1500);
 			} else{
-				$("#Opening .areaOpening").removeClass("up");
+				$("#section2").removeClass("step1");
+				$("#section2").removeClass("step2");
+				$("#section2").removeClass("step3");
+				$("#section2").removeClass("step4");
+				$("#section2").removeClass("step5");
+
 			}
 		}
 	});
